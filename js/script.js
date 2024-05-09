43b9b22d1a53891d1b1b1bccda038229461b24c0
MFirdausA <mfirdausakbar88@gmail.com>
2024-05-09 22:56:18 +0700

    add feature and style

--- js/script.js
+++ js/script.js
@@ -1,22 +1,70 @@
-// ini js
-
+// form-validation
 function validation() {
-    console.log("good")
+    let username = document.getElementById('form-username').value;
+    let email = document.getElementById('formEmail').value;
+    let interest = document.getElementById('optionForm').value;
+    console.log(username);
+    console.log(email);
+    console.log(interest);
+    if (username == '' || username == null) {
+        document.getElementById('form-username').style.border = '1px solid red';
+        alert('Username anda kosong, periksa kembali');
+    } else {
+
+    }
+    if (email == '' || email == null) {
+        document.getElementById('formEmail').style.border = '1px solid red';
+        alert('Email anda kosong, periksa kembali');
+
+    } else {
+        if (!isValidEmail(email)) {
+            alert("Email anda tidak valid!, periksa kembali");
+            return false;
+        }
+    }
+
+    if (interest == '' || interest == null) {
+        document.getElementById('optionForm').style.border = '1px solid red';
+        alert('Option anda kosong, periksa kembali');
+    } else {
+
+    }
+}
+// send button
+document.getElementById('send-btn').addEventListener('click', () => validation())
+// validation email
+function isValidEmail(email) {
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
 }
 
-document.getElementById('send-btn').addEventListener('click', () => validation())
+//send to local storage
+function sendContact() {
+    let username = document.getElementById('form-username').value;
+    let email = document.getElementById('formEmail').value;
+    let interest = document.getElementById('optionForm').value;
 
+    if (username && email && interest) {
+        alert(`${username}, thanks for registering.`);
+        // Save form data to localStorage
+        localStorage.setItem('form-username', username);
+        localStorage.setItem('formEmail', email);
+        localStorage.setItem('optionForm', interest);
 
+        // Display the saved data on the webpage
+        document.getElementById('display').innerHTML = `
+            <p>Your Name: ${username}</p>
+            <p>Email Address: ${email}</p>
+            <p>Interested in: ${interest}</p>
+        `;
+    } else {
+        alert("There was an error. Please fill in all fields.");
+    }
+}
+
+// auto slide
 let slideIndex = 0;
 showSlides();
-
-// function plusSlides(n) {
-//     showSlides(slideIndex -= n);
-// }
-
-// function currentSlide(n) {
-//     showSlides(slideIndex = n);
-// }
 
 function showSlides() {
     let i;
@@ -35,4 +83,4 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 3500); // Change image every 3,5 seconds
-}
+}   
